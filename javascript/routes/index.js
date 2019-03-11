352e7a3ad1d8307e8ed6db65b4feca9420f683fc
Fig Yogurt <itisaftab@gmail.com>
2019-03-10 22:23:04 -0400

    install request and obtain city and state from form

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var us_states = require("../us_state.js");
+const request = require("request");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -8,7 +9,10 @@ router.get("/", function(req, res, next) {
 });
 
 router.post("/search", function(req, res, next) {
-  res.send("<h1>Test</h1>");
+  // res.send("<h1>Test</h1>");
+  const city = req.body.city.toLowerCase().replace(/ /g, "_");
+  const state = req.body.state.toLowerCase();
+  const url = `https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:${state},ocd-division/country:us/state:${state}/place:${city}}`;
 });
 
 module.exports = router;
