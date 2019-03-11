b320b42dcf45272994a1f601ecb2d113c26a56de
Fig Yogurt <itisaftab@gmail.com>
2019-03-10 23:00:06 -0400

    made a request route but it doesnt seem to turn the data into a json

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var us_states = require("../us_state.js");
 const request = require("request");
+const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -13,6 +14,27 @@ router.post("/search", function(req, res, next) {
   const city = req.body.city.toLowerCase().replace(/ /g, "_");
   const state = req.body.state.toLowerCase();
   const url = `https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:${state},ocd-division/country:us/state:${state}/place:${city}}`;
+  console.log(url);
+
+  request.get(
+    {
+      url,
+      header: {
+        Accept: "application/json"
+      }
+    },
+    function(err, response, body) {
+      console.log(response);
+      console.log(body);
+      if (err) {
+        console.log(err);
+        res.redirect("/");
+      } else {
+        console.log("hi");
+        res.send("<h1>Test</h1>");
+      }
+    }
+  );
 });
 
 module.exports = router;
