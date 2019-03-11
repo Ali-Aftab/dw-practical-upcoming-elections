a5bcff128d33fa183aedf4ec5693cebdbb6dd7ef
Fig Yogurt <itisaftab@gmail.com>
2019-03-10 22:15:29 -0400

    route made

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -1,10 +1,14 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var us_states = require('../us_state.js');
+var us_states = require("../us_state.js");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Find My Election', states: us_states });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Find My Election", states: us_states });
+});
+
+router.post("/search", function(req, res, next) {
+  res.send("<h1>Test</h1>");
 });
 
 module.exports = router;
