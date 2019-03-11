40b4eecfa03de410cb48b354541ba3b2574721f9
Fig Yogurt <itisaftab@gmail.com>
2019-03-11 08:35:26 -0400

    fixed typos and added json to HTML format if request api bug is fixed

--- javascript/routes/index.js
+++ javascript/routes/index.js
@@ -2,13 +2,13 @@ var express = require("express");
 var router = express.Router();
 var us_states = require("../us_state.js");
 const request = require("request");
-const fs = require("fs");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "Find My Election", states: us_states });
 });
 
+/* POST search */
 router.post("/search", function(req, res, next) {
   // res.send("<h1>Test</h1>");
   const city = req.body.city.toLowerCase().replace(/ /g, "_");
@@ -18,24 +18,65 @@ router.post("/search", function(req, res, next) {
   //typing up the url will provide an edn file but my code doesnt seem to obtain that nor the json version of it, tried many different ways
   //the json i should have received should have looked like this https://repl.it/repls/PaleCyberSystemsoftware
 
-  request.get(
+  request(
     {
       url,
-      header: {
+      headers: {
         Accept: "application/json"
-      }
+      },
+      method: "GET"
     },
     function(err, response, body) {
+      console.log(response);
+      console.log(body);
       if (err) {
         console.log(err);
         res.redirect("/");
       } else {
         const result = body;
-        //if I had more time I would have done a for loop and place the text in its proper <tag>
-        res.send("<h1>Test</h1>");
+        const htmlFormat = jsonToHtml(result);
+        res.send(htmlFormat);
       }
     }
   );
 });
+
+// turn json to html
+function jsonToHtml(result) {
+  let election = "";
+  for (let i = 0; i < result.length; i++) {
+    let districtObj = result[i];
+    for (let key in districtObj) {
+      if (typeof districtObj[key] === "object") {
+        isObject(districtObj[key], election);
+      } else {
+        election =
+          election +
+          `
+      <h3>${key}</h3>
+      <br />
+      <p>${districtObj[key]}</p>
+      <br />
+      `;
+      }
+    }
+  }
+}
+function isObject(info, str) {
+  for (let key in info) {
+    if (typeof info[key] === "object") {
+      isObject(info[key], str);
+    } else {
+      str =
+        str +
+        `
+      <h3>${key}</h3>
+      <br />
+      <p>${info[key]}</p>
+      <br />
+      `;
+    }
+  }
+}
 
 module.exports = router;
